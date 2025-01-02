75ae202e29f3912867112b24f3bb5772d228b05a
koplakzs <atom.go.76@gmail.com>
2025-01-02 16:25:40 +0700

    need fixed cors delete

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -16,6 +16,9 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->alias([
             "role" => RoleMiddleware::class
         ]);
+        $middleware->use([
+            \Illuminate\Http\Middleware\HandleCors::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
